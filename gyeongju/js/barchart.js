a329c56ae3f5a9fa28e817b6b55a47479339e165
ginjae <110318875+ginjae@users.noreply.github.com>
2024-07-31 15:28:54 +0900

    Add Main Page

---
+++ gyeongju/js/barchart.js
@@ -0,0 +1,147 @@
+class barChart {
+
+  constructor() {
+    this.data = null;
+    this.categoryCounts = null;
+    this.cScale = null;
+  }
+
+  initData(data) {
+    try {
+      var categoryCounts = [];
+      var category = [];
+      for (var d of data) {
+        if (category.includes(d["업태구분명"])) {
+          categoryCounts.find(obj => obj["category"] == d["업태구분명"]).count += 1;
+        }
+        else {
+          categoryCounts.push({ category: d["업태구분명"], count: 1 });
+          category.push(d["업태구분명"]);
+        }
+      }
+      categoryCounts = categoryCounts.sort((a, b) => b.count - a.count);
+      // console.log(data);
+      // console.log(categoryCounts);
+
+      this.data = data;
+      this.categoryCounts = categoryCounts;
+      this.cScale = d3.scaleOrdinal()
+      .domain(category)
+      .range(["#4E79A7", "#7B848F", "#A0CBE8", "#F28E2B", "#FFBE7D", "#59A14F", "#8CD17D", "#B6992D", "#F1CE63", "#499894", "#86BCB6",
+        "#E15759", "#FF9D9A", "#79706E", "#BAB0AC", "#D37295", "#FABFD2", "#B07AA1", "#D4A6C8", "#9D7660", "#D7B5A6"]);
+      this.drawBarChart(categoryCounts);
+    }
+    catch(error) {
+      console.error(error);
+    }
+  }
+
+  drawBarChart(data) {
+
+    var category = d3.map(data, function(d) { return d.category; });
+
+    const margin = { top: 5, right: 30, bottom: 120, left: 60 },
+      width = 800 - margin.left - margin.right,
+      height = 600 - margin.top - margin.bottom;
+
+    const svg = d3.select("#barchart")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    const xScale = d3.scaleBand()
+      .domain(category)
+      .range([0, width]);
+    
+    const yScale = d3.scaleLinear()
+      .domain([0, 1 + 1.1 * d3.max(data, d => { return d.count; })])
+      .range([height, 0]);
+
+    var bar = svg.append("g")
+      .selectAll(".bar")
+      .data(data)
+      .enter();
+
+    const bar_width = width * 0.8 / category.length;
+    bar.append("rect")
+      .attr("class", "bar")
+      .attr("fill", d => { return this.cScale(d.category); })
+      .attr("x", d => { return bar_width / 7 + xScale(d.category); })
+      .attr("y", yScale(0))
+      .attr("width", bar_width);
+      // .attr("width", 20)
+      // .attr("height", d => { return height - yScale(d.count); });
+    bar.selectAll("rect")
+      .transition()
+      .duration(500)
+      .ease(d3.easeCubicInOut)
+      .attr("y", d => { return yScale(d.count); })
+      .attr("height", d => { return height - yScale(d.count); });
+    bar.append("text")
+      .text(d => { return d.count; })
+      .attr("x", d => { return bar_width / 7 + bar_width / 2 + xScale(d.category); })
+      .attr("y", yScale(0))
+      .attr("text-anchor", "middle")
+      .attr("font-size", 10);
+    bar.selectAll("text")
+      .transition()
+      .duration(500)
+      .ease(d3.easeCubicInOut)
+      .attr("y", d => { return yScale(d.count) - 3; })
+      .attr("height", d => { return height - yScale(d.count); });
+
+
+      const xAxis = d3.axisBottom(xScale);
+      const yAxisTicks = yScale.ticks()
+        .filter(tick => Number.isInteger(tick));
+      const yAxis = d3.axisLeft(yScale)
+        .tickValues(yAxisTicks)
+        .tickFormat(d3.format('d'));
+
+      svg.append("g")
+          .attr("class", "x-axis")
+          .attr("transform", `translate(0, ${height})`)
+          .call(xAxis)
+          .selectAll("text")
+          .attr("transform", "rotate(-45)")
+          .attr("text-anchor", "end");
+
+      svg.append("g")
+          .attr("class", "y-axis")
+          .call(yAxis);
+
+  }
+
+  filterBarDataByRegion(regionList) {
+    var categoryCounts = this.categoryCounts;
+    categoryCounts.forEach(d => { d.count = 0; });
+    if (regionList.length == 0) {
+      for (var d of this.data) {
+        categoryCounts.find(obj => obj["category"] == d["업태구분명"]).count += 1;
+      }
+      categoryCounts = categoryCounts.sort((a, b) => b.count - a.count);
+      d3.select("#barchart").select("svg").remove();
+      bar.drawBarChart(categoryCounts)
+    }
+    else {
+      var filteredData = this.data.filter(d => {
+        var isSelected = false;
+        for (var region of regionList) {
+          if (d['소재지전체주소'].includes(region)) {
+            isSelected = true;
+            break;
+          }
+        }
+        return isSelected;
+      })
+      for (var d of filteredData) {
+        categoryCounts.find(obj => obj["category"] == d["업태구분명"]).count += 1;
+      }
+      categoryCounts = categoryCounts.sort((a, b) => b.count - a.count);
+      d3.select("#barchart").select("svg").remove();
+      bar.drawBarChart(categoryCounts)
+    }
+  }
+}
---
+++ gyeongju/js/map.js
@@ -0,0 +1,189 @@
+const margin = { top: 5, right: 30, bottom: 120, left: 120 },
+  width = 800,
+  height = 600;
+
+const svg = d3.select("#map")
+  .append("svg")
+  .attr("id", "maps")
+  .attr("width", width)
+  .attr("height", height)
+  .style("background-color", "#FAFAFA")
+  .style("border-radius", "10px")
+  .style("border", "1.5px solid black");
+
+//const getColor = () => "#" + Math.round(Math.random() * 0xffffff).toString(16);
+
+// const clip = svg.append("defs").append("clipPath")
+//   .attr("id", "clip")
+//   .append("rect")
+//   .attr("width", width)
+//   .attr("height", height);
+
+const zoom = d3.zoom()
+  .scaleExtent([.5, 10000])
+  .on("zoom", updateMap);
+
+// svg.append("rect")
+//   .attr("width", width)
+//   .attr("height", height)
+//   .style("fill", "black")
+//   .style("pointer-events", "all")
+//   .call(zoom);
+
+svg.call(zoom)
+  .on("dblclick.zoom", null);
+
+const projection = d3.geoMercator()
+  .center([129.2270222, 35.85316944])
+  .scale(50000)
+  // .scale(100000)
+  .translate([width / 2, height / 2]);
+
+const geoPath = d3.geoPath().projection(projection);
+
+var regions = [];
+var selectedRegion = [];
+
+d3.json("data/Gyeongju.geojson").then((data) => {
+  regions = data.features.map(d => { return d.properties.EMD_KOR_NM; });
+  regions.sort((a, b) => b.length - a.length);
+  d3.select("#maps")
+    // .append("g")
+    // .attr("clip-path", "url(#clip)")
+    .append("g")
+    .selectAll("path")
+    .data(data.features)
+    .enter()
+    .append("path")
+    .attr("fill", "#EAEAEA")
+    .attr("fill-opacity", "0.4")
+    .attr("d", geoPath)
+    .attr("class", d => { return d.properties.EMD_KOR_NM; })
+    .attr("transform", "rotate(-1.5,400,300)")
+    .style("stroke", "black")
+    .style("stroke-width", "0.3px")
+    .on("click", function(event, d) {
+      const isSelected = d3.select(this).attr("class") === d.properties.EMD_KOR_NM + "selected";
+      if (isSelected) {
+        d3.select(this).attr("class", d.properties.EMD_KOR_NM);
+        d3.select(this)
+          .transition()
+          .duration(200)
+          .attr("fill", "#EAEAEA");
+          // .attr("fill", "steelblue");
+        selectedRegion.splice(selectedRegion.indexOf(d.properties.EMD_KOR_NM), 1);
+        // scatter.RemoveDotsByRegion(d.properties.EMD_KOR_NM);
+
+        if (selectedRestaurant !== null) {
+          if (selectedRestaurant["소재지전체주소"].includes(d.properties.EMD_KOR_NM)) {
+            selectedRestaurant = null;
+            d3.select("#maps")
+              .selectAll("#dot" + d.properties.EMD_KOR_NM)
+              .style("stroke", "none");
+          }
+        }
+      }
+      else {
+        d3.select(this).attr("class", d.properties.EMD_KOR_NM + "selected");
+        d3.select(this)
+          .transition()
+          .duration(200)
+          .attr("fill", "steelblue");
+          // .attr("fill", "purple");
+        selectedRegion.push(d.properties.EMD_KOR_NM);
+        scatter.AddDotsByRegion(d.properties.EMD_KOR_NM, true);
+      }
+
+      if (selectedRestaurant === null) {
+        if (selectedRegion.length === 0) {
+          info.html("<b>상세정보</b><br>[읍, 면, 동] 선택 후, 사업장을 선택해주세요.");
+          repositionInfo();
+        }
+        else {
+          info.html("<b>상세정보</b><br> 사업장을 선택해주세요.");
+          repositionInfo();
+        }
+      }
+      bar.filterBarDataByRegion(selectedRegion);
+      // scatter.filterScatterDataByRegion(selectedRegion);
+    })
+    .on("mouseover", function(event, d) {
+      showTooltip(event, d);
+      const isSelected = d3.select(this).attr("class") === d.properties.EMD_KOR_NM + "selected";
+      d3.select(this)
+        .raise()
+        .style("stroke-width", "0.6px")
+        .style("stroke", "red");
+      scatter.AddDotsByRegion(d.properties.EMD_KOR_NM, false);
+      // if (isSelected)
+      //   d3.select(this).attr("fill", "purple");
+      // else 
+      //   d3.select(this).attr("fill", "steelblue");
+    })
+    .on("mousemove", moveTooltip)
+    .on("mouseout", function(event, d) {
+      hideTooltip(event, d);
+      const isSelected = d3.select(this).attr("class") === d.properties.EMD_KOR_NM + "selected";
+      d3.select(this)
+        .style("stroke-width", "0.3px")
+        .style("stroke", "black");
+      if (!isSelected) {
+        scatter.RemoveDotsByRegion(d.properties.EMD_KOR_NM);
+      }
+      // if (isSelected)
+      //   d3.select(this).attr("fill", "red");
+      // else 
+      //   d3.select(this).attr("fill", "#EAEAEA");
+    });
+
+});
+
+function updateMap(event) {
+  d3.select("#maps")
+    .selectAll("path")
+    .attr("transform", event.transform + "rotate(-1.5,400,300)");
+
+  tooltip
+    .style("left", (event.sourceEvent.clientX + 10) + "px")
+    .style("top", (event.sourceEvent.clientY + 10) + "px");
+
+  scatter.updateScatterPlot(event);
+}
+
+
+const tooltip = d3.select("#map")
+  .append("div")
+  .style("opacity", 0)
+  .attr("class", "tooltip")
+  .style("background-color", "black")
+  .style("border-radius", "5px")
+  .style("padding", "5px")
+  .style("color", "white")
+  .style("display", "inline")
+  .style("position", "fixed")
+  .style("pointer-events", "none")
+  .style("font-size", "10px");
+
+function showTooltip(event, d) {
+  tooltip
+    .transition()
+    .duration(10)
+    .style("opacity", 1);
+  tooltip
+    .html(d.properties.EMD_KOR_NM)
+    .style("left", (event.clientX + 10) + "px")
+    .style("top", (event.clientY + 10) + "px");
+}
+
+function moveTooltip(event, d) {
+  tooltip
+    .style("left", (event.clientX + 10) + "px")
+    .style("top", (event.clientY + 10) + "px");
+}
+
+function hideTooltip(event, d) {
+  tooltip
+    .transition()
+    .duration(200)
+    .style("opacity", 0);
+}
